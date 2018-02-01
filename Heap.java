4b33faf783c0cd26fcc97e985ddab08b28bca15e
Julio Berina <julioberina@gmail.com>
2018-02-01 09:27:57 -0800

    Added a menu to main with a baseline Heap class

---
+++ Heap.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+
+/*
+  This class represents the heap data structure with all of its methods
+ */
+public class Heap
+{
+	private int[] hArray; // array storing heap data
+	
+	/*
+	  Default constructor for creating heap. Allocates 101 spaces for array as max heap size
+	 */
+	public Heap()
+	{
+		hArray = new int[101];
+	}
+}
--- Project2.java
+++ Project2.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
+
+/*
+  This class represents the main class for the entire project containing the main method
+ */
 public class Project2
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Heap Project coming soon...");
+		Scanner scan = new Scanner(System.in);
+		int choice = 0;
+		
+		System.out.println("Please select how to test the program:\n");
+		System.out.println("(1) 20 sets of 100 randomly generated integers");
+		System.out.println("(2) Fixed integer values 1-100\n");
+		System.out.print("Enter choice:  ");
+
+		choice = scan.nextInt();
+
+		switch (choice)
+		{
+		case 1:
+			System.out.println("You chose 1");
+			break;
+		case 2:
+			System.out.println("You chose 2");
+			break;
+		default:
+			System.out.println("Invalid input!  Exiting...");
+		}
 	}
 }
